d2a8c3974eb6eda7eff7fdaeb9c5cb59e13c04ba
Corentin Saint-Dizier <corentinstd@gmail.com>
2020-12-14 15:48:13 +0100

    Ajout du model item

--- src/model/Item.php
+++ src/model/Item.php
@@ -1,2 +1,16 @@
 <?php
 
+namespace mywishlist\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model{
+
+    protected $table = 'item';
+    protected $primaryKey = 'id';
+    public $timestamps= false;
+
+    public function list(){
+        return $this -> belongsTo('mywishlist\models\Liste', 'liste_id');
+    }
+}
